c109db53c2422bf8c6bd65074f34769d0b3f23d6
CaiquedoVale <caiquedovale_@hotmail.com>
2025-11-08 10:26:48 -0300

    feat: initial commit

---
+++ src/main/java/com/example/projeto_a3_sistemas_distribuidos_mobile_api/ProjetoA3SistemasDistribuidosMobileApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_a3_sistemas_distribuidos_mobile_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoA3SistemasDistribuidosMobileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoA3SistemasDistribuidosMobileApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto_a3_sistemas_distribuidos_mobile_api/ProjetoA3SistemasDistribuidosMobileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_a3_sistemas_distribuidos_mobile_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoA3SistemasDistribuidosMobileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
